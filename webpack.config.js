3ef07eff60bf34a7f4686dfde8d0107f39f4f74d
wadezhu <wade.chu@yxunistar.com>
2019-01-30 16:34:06 +0800

    add yarn build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/scripts/index.js',
     output: {
-        filename: 'index.bundle.js',
-        path: path.resolve(__dirname, './'),
+        filename: 'scripts/main.js',
+        path: path.resolve(__dirname, './dist'),
     }
 }
